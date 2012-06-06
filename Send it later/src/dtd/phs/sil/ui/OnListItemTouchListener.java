33c1c43613712417640103b2dc76e3f45050a97e
sphamhung@gmail.com <sphamhung@gmail.com@d6603cc4-3032-1062-a61c-6431ac2c0fd1>
2012-06-06 03:39:41 +0000

    - Done: fix bug onSwipe not called

--- Send it later/src/dtd/phs/sil/ui/OnListItemTouchListener.java
+++ Send it later/src/dtd/phs/sil/ui/OnListItemTouchListener.java
@@ -6,9 +6,9 @@ import android.view.View.OnTouchListener;
 import dtd.phs.sil.utils.Logger;
 
 public abstract class OnListItemTouchListener implements OnTouchListener {
-	final private float MIN_X_SWIPE = 30;
+	final private float MIN_X_SWIPE = 60;
 	final private float MAX_Y_SWIPE = 120;
-	final private float MIN_VELOCITY = 0;
+	final private float MIN_VELOCITY = 300;
 	protected static final float MAX_CLICK = 10;
 	protected static final long CLICK_TIME = 400;
 	private static final long MIN_LONG_CLICK_TIME = 1000;
@@ -31,22 +31,27 @@ public abstract class OnListItemTouchListener implements OnTouchListener {
 
 	@Override
 	public boolean onTouch(View v, MotionEvent event) {
+		long dtime = 0;
 		switch (event.getAction()) {
 		case MotionEvent.ACTION_DOWN:
-			Logger.logInfo("ACtion down !");
+			
 			startX = event.getX();
 			startY = event.getY();
 			startTime = System.currentTimeMillis();
+			Logger.logInfo("ACtion down, time: " + startTime);
 			return true;
+		case MotionEvent.ACTION_CANCEL:
+			Logger.logInfo("ACtion cancel, time: " + System.currentTimeMillis());
 		case MotionEvent.ACTION_UP:
-			Logger.logInfo("ACtion Up!");
 			endX = event.getX();
 			endY = event.getY();
 			endTime = System.currentTimeMillis();
+			Logger.logInfo("ACtion up, time: " + endTime);
 			float dx = Math.abs(endX - startX);
 			float dy = Math.abs(endY - startY);
-			long dtime = endTime - startTime;
-			float velo = (float) (Math.sqrt(dx*dy + dy*dy) / dtime );
+			dtime = endTime - startTime;
+			float velo = (float) (Math.sqrt(dx*dx + dy*dy) / (0.001*dtime));
+			Logger.logInfo("(DX,DY,Velo) at action up: (" + dx + "," + dy + "," + velo);
 			if ( dx > MIN_X_SWIPE && dy < MAX_Y_SWIPE && velo > MIN_VELOCITY) {
 				Logger.logInfo("OnSwipe is called !");
 				onSwipe(this.view,position);
@@ -56,14 +61,9 @@ public abstract class OnListItemTouchListener implements OnTouchListener {
 					onClick(this.view,position);
 					return true;
 				} 
-//				else {
-//					onLongClick(position);
-//					return true;
-//				}
 			}
 			return false;
-		case MotionEvent.ACTION_CANCEL:
-			return false;
+			
 		case MotionEvent.ACTION_MOVE:
 			float x = event.getX();
 			float y = event.getY();
@@ -71,8 +71,6 @@ public abstract class OnListItemTouchListener implements OnTouchListener {
 			dy = Math.abs(y - startY);
 			dtime = System.currentTimeMillis() - startTime;
 			if ( dx < MAX_CLICK && dy < MAX_CLICK && dtime >= MIN_LONG_CLICK_TIME ) {
-//				vib = (Vibrator) (v.getContext()).getSystemService(Context.VIBRATOR_SERVICE);
-//				vib.vibrate(VIBRATOR_PERIOD);
 				onLongClick(position);
 				return true;
 			}
